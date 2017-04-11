a095dbd5545fa3f61acc48c363f1dcb0284ef760
Martino Mancino <martino.mancino@babylonhealth.com>
2017-04-11 15:10:39 +0100

    First commit

---
+++ src/components/checkbox/CheckboxContainer.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import Checkbox from './Checkbox';
+
+
+class CheckboxContainer extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <h5>Amazing React Checkbox</h5>
+        <Checkbox/>
+      </div>
+    )
+  }
+}
+
+export default CheckboxContainer;
